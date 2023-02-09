472d3484d8f110ef2b91080897c6d47a86929fce
Mahdi Mohaqeq <mahdi.mohaqeq.mm@gmail.com>
2023-02-09 18:27:37 +0330

    [IMPROVE] Exapndable styles

--- src/components/UsersList/UserCard/UserExpandable/index.tsx
+++ src/components/UsersList/UserCard/UserExpandable/index.tsx
@@ -18,25 +18,28 @@ function UserExapndable({
 }: IUserExapndableProps) {
   return (
     <div
-      className={`px-4 py-2 rounded-lg border border-gray-200 flex items-center justify-between transition-all duration-300 ${customClass} ${
+      className={`px-4 py-2 rounded-lg border border-gray-200 transition-all duration-300 ${customClass} ${
         expanded
           ? "max-h-screen visible opacity-100 mt-2"
           : "max-h-0 invisible !px-0 !py-0 opacity-0 mt-0"
       }`}
     >
-      <h2 className="text-xl font-bold italic text-emerald-500 select-none">
-        {user.name?.toUpperCase()} Albums
-      </h2>
-      <Button
-        className="w-[15ch]"
-        onClick={(e) => {
-          e.stopPropagation();
-          //   if (!loading) runThunk();
-        }}
-      >
-        <BsFillPlusSquareFill className="w-4 h-4" />
-        <span>Add Album</span>
-      </Button>
+      <div className={` flex items-center justify-between `}>
+        <h2 className="text-xl font-bold italic text-emerald-500 select-none">
+          {user.name?.toUpperCase()} Albums
+        </h2>
+        <Button
+          className="w-[15ch]"
+          onClick={(e) => {
+            e.stopPropagation();
+            //   if (!loading) runThunk();
+          }}
+        >
+          <BsFillPlusSquareFill className="w-4 h-4" />
+          <span>Add Album</span>
+        </Button>
+      </div>
+      <div className="h-[0.125rem] bg-gradient-to-r from-[#34d39900] via-[#34d399] to-[#34d39900] my-4" />
     </div>
   );
 }
